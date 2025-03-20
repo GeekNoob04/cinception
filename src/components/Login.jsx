579035498094185123849f898c08cb946f6314b9
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-03-20 23:47:44 +0530

    bg css update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,9 +31,11 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
+        <div className="absolute inset-0 bg-black opacity-50"></div>
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/f6e7f6df-6973-46ef-b98f-12560d2b3c69/web/IN-en-20250317-TRIFECTA-perspective_26f87873-6014-460d-a6fb-1d96d85ffe5f_large.jpg"
           alt="background image"
+          className="w-full"
         />
       </div>
       <form
