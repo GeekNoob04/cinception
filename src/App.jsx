61ff45d5e80d2ce91cdc98515050f751290eaeae
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-03-20 21:18:04 +0530

    sign in/up form & Login/Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Body from "./components/Body";
+
 function App() {
-  return (
-      <div className="text-3xl font-bold text-green-600">
-       Namaste cinegpt
-    </div>
-  )
+  return <Body />;
 }
 
-export default App
+export default App;
