91ee3756d939933e66cecf0b114a37f5ed67e23a
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-03-20 23:49:39 +0530

    bg update css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,6 @@ const Login = () => {
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/f6e7f6df-6973-46ef-b98f-12560d2b3c69/web/IN-en-20250317-TRIFECTA-perspective_26f87873-6014-460d-a6fb-1d96d85ffe5f_large.jpg"
           alt="background image"
-          className="w-full"
         />
       </div>
       <form
